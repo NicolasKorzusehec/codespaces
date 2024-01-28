60d21bfc6b509c7f14c2a4543d57e6bbd30686bf
Nicolás Korzusehec <99510894+NicolasKorzusehec@users.noreply.github.com>
2024-01-28 05:49:06 +0000

    comentarios

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 sudo apt-get update
 sudo apt-get install sl
 
+#! Al escribir el siguiente comando courre una animacion
+#! /usr/games/sl
+
 #!commit commands:
 #!git add setup.sh --chmod=+x
 #!git commit -m "Adding setup.sh from the codespace!"
